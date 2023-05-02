369639d0f5ec8b7c9fc76af8bbdd1fc667dfaaeb
jayasaisuryan <129358868+jayasaisuryan@users.noreply.github.com>
2023-05-02 15:24:10 +0530

    Update conversion.py

--- conversion.py
+++ conversion.py
@@ -1,3 +1,11 @@
-import os
-import openai
+from os import path
 from pydub import AudioSegment
+
+#importing audio file
+src = "transcript.mp3"
+dst = "test.wav"
+
+#convert wav to mp3                                                            
+sound = AudioSegment.from_mp3(src)
+sound.export(dst, format="wav")
+
